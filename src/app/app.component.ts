cd3ddf0887645a72a7dde1c2e29f75452c07f205
arpan <arpan@wrctpl.com>
2020-06-12 19:53:25 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title = 'Counter App';
+  count = 0;
+  countReset(){
+    this.count = 0; 
+  }
+  countIncrease(){
+    this.count = this.count + 1; 
+    if(this.count > 10){
+      this.count = 10;
+    }
+  }
+  countDecrease(){
+    this.count = this.count - 1; 
+    if(this.count < 0){
+      this.count = 0;
+    }
+  }
 }
