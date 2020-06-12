70e1567ab0d7d8b4a8ef0fd7bbad02af67d715cf
arpan <arpan@wrctpl.com>
2020-06-12 20:25:41 +0530

    committed by Arpan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-root',
@@ -15,12 +17,22 @@ export class AppComponent {
     this.count = this.count + 1; 
     if(this.count > 10){
       this.count = 10;
+      Swal.fire({
+        title: 'Not possible',
+        text: 'count can not increase now',
+        icon: 'warning'
+      });
     }
   }
   countDecrease(){
     this.count = this.count - 1; 
     if(this.count < 0){
       this.count = 0;
+      Swal.fire({
+        title: 'Not possible',
+        text: 'count can not decrease now',
+        icon: 'warning'
+      });
     }
   }
 }
